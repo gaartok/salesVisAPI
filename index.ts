2bff6f02c5c483b0181b0c6b959aae6e7d4faadb
ddurant <ddurant@thoughtwaves.com>
2022-07-18 09:11:26 -0400

    added 404 page

--- index.ts
+++ index.ts
@@ -11,6 +11,18 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(fileUpload({ createParentPath: true }));
 app.use('/', routes);
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
+});
+
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
+
+app.get('*', function(req, res){
+  res.status(404).render('404', { title: '404' });
+  res.send('what???');
+});
 
 mongoose.connect(process.env.MONGODB_URI)
 .catch(error => { console.log(`error = ${JSON.stringify(error)}`); })
